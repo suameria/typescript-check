736875b73c9d6005f6dfa741fbe55c88f6706d8d
Hiroshi.Arai <suameria@gmail.com>
2024-03-08 15:43:02 +0900

    hello world

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,37 @@
-import { join } from "path";
-
-export const entry = {
-  bundle: "./src/index.ts",
-};
-
-export const output = {
-  path: join(__dirname, "dist"),
-  // entry:{bundle:}のbundleが名前となるのでbundle.js
-  filename: "[name].js",
-};
-
-export const resolve = {
-  extensions: [".ts", ".js"],
-};
-
-export const devServer = {
-  contentBase: join(__dirname, "dist"),
-  open: true,
-};
-
-export const module = {
-  rules: [
-    {
-      loader: "ts-loader",
-      test: /\.ts$/,
+const path = require("path");
+module.exports = {
+  // モジュールバンドルを行う起点となるファイルの指定
+  // 指定できる値としては、ファイル名の文字列や、それを並べた配列やオブジェクト
+  // 下記はオブジェクトとして指定した例
+  entry: {
+    bundle: "./src/index.ts",
+  },
+  // モジュールバンドルを行った結果を出力する場所やファイル名の指定
+  output: {
+    path: path.join(__dirname, "dist"), // "__dirname"はファイルが存在するディレクトリ
+    filename: "[name].js", // [name]はentryで記述した名前（この設定ならbundle）
+  },
+  // import文でファイル拡張子を書かずに名前解決するための設定
+  // 例...「import World from './world'」と記述すると"world.ts"という名前のファイルをモジュールとして探す
+  resolve: {
+    extensions: [".ts", ".js"], // Reactの.tsxや.jsxの拡張子も扱いたい場合は配列内に追加する
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"), // webpack-dev-serverの公開フォルダ
     },
-  ],
+    // ポート指定
+    port: 3000,
+  },
+  // モジュールに適用するルールの設定（ローダーの設定を行う事が多い）
+  module: {
+    rules: [
+      {
+        // 拡張子が.tsのファイルに対してTypeScriptコンパイラを適用する
+        // Reactで用いる.tsxの拡張子にも適用する場合は test:/\.(ts|tsx)$/,
+        test: /\.ts$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
 };
